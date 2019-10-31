6d1bfa4876722d6b9a405d5a784c9023252bad64
Long <long.zhang@pb.com>
2019-10-31 15:18:34 +0800

    first commit

---
+++ app/src/main/java/com/example/jetpackdemo/LoginFragment.kt
@@ -0,0 +1,25 @@
+package com.example.jetpackdemo
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_login.*
+
+class LoginFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val bundle = Bundle()
+        et_account.setText(bundle.getString("name"))
+    }
+}
