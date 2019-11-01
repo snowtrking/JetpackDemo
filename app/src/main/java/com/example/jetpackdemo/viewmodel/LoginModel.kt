e4a1b9663de463201196adc82ba6a6a76610be05
Long <long.zhang@pb.com>
2019-11-01 18:21:18 +0800

    add

---
+++ app/src/main/java/com/example/jetpackdemo/viewmodel/LoginModel.kt
@@ -0,0 +1,33 @@
+package com.example.jetpackdemo.viewmodel
+
+import android.content.Context
+import android.content.Intent
+import android.widget.Toast
+import androidx.databinding.ObservableField
+import androidx.navigation.navOptions
+import com.example.jetpackdemo.common.BaseConstant
+import com.example.jetpackdemo.ui.activity.MainActivity
+
+class LoginModel constructor(name: String, pwd: String, context: Context) {
+    val n = ObservableField<String>(name)
+    val p = ObservableField<String>(pwd)
+    var context: Context = context
+
+    fun onNameChanged(s: CharSequence) {
+        n.set(s.toString())
+    }
+
+    fun onPwdChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+        p.set(s.toString())
+    }
+
+    fun login() {
+        if (n.get().equals(BaseConstant.USER_NAME)
+            && p.get().equals(BaseConstant.USER_PWD)
+        ) {
+            Toast.makeText(context, "账号密码正确", Toast.LENGTH_SHORT).show()
+            val intent = Intent(context, MainActivity::class.java)
+            context.startActivity(intent)
+        }
+    }
+}
